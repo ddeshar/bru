2159649084f048c851d3986cace896d4a2d45629
NAN-NB\NAN <ampare4444@gmail.com>
2017-12-08 16:30:19 +0700

    แก้ไขยื่่นกู้

--- admin/admin_submitted_add.php
+++ admin/admin_submitted_add.php
@@ -80,7 +80,9 @@ require_once('include/_header.php');
 											<?php
 												if (isset($_GET["loan"])) {
 													$loan = $_GET["loan"];
-													$sql = "SELECT mem_id,fak_total, MAX(fak_date) AS fak_date FROM deposit WHERE mem_id = $loan GROUP BY fak_total  desc LIMIT 1";
+													$sql = "SELECT mem_id,fak_total,withdraw, MAX(fak_date) AS fak_date FROM deposit WHERE mem_id = $loan GROUP BY fak_id  desc LIMIT 1";
+
+                          // echo $sql;
 													$result = mysqli_query($link, $sql);
 													$row = mysqli_fetch_assoc($result);
 													$budget = $row["fak_total"];
